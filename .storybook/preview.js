e77a1904c2c58532234a2d6a7d240638a95a1ef6
Stefan Natter <stefan@natter.at>
2021-04-04 10:19:26 +0200

    feat: added storybook-addon-next-router

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
+import { withNextRouter } from 'storybook-addon-next-router'
+
 import '../src/styles/index.scss'
+
+// @see https://www.npmjs.com/package/storybook-addon-next-router
+export const decorators = [
+  withNextRouter({
+    path: '/', // defaults to `/`
+    asPath: '/', // defaults to `/`
+    query: {}, // defaults to `{}`
+    push() {}, // defaults to using addon actions integration, can override any method in the router
+  }),
+]
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
