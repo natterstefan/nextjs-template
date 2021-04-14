0ab162dcdd6c8b65a9c93543704104f73e2576d9
Stefan Natter <stefan@natter.at>
2021-04-14 20:14:51 +0200

    chore: typed nextConfig

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const withPlugins = require('next-compose-plugins')
 const { i18n } = require('./next-i18next.config')
 const version = require('./version')
 
+/**
+ * // @type {import('next').Config}
+ * TODO: make type above work
+ * - @see https://github.com/vercel/next.js/issues/8044#issuecomment-525054841
+ * - @see https://github.com/vercel/next.js/pull/13429#issuecomment-634621879
+ * – It should be availabe already: https://github.com/vercel/next.js/blob/canary/packages/next/next-server/server/config-shared.ts#L12
+ */
 const nextConfig = {
   poweredByHeader: false,
   /**
--- src/types/next.d.ts
+++ src/types/next.d.ts
@@ -1,21 +1,4 @@
 import { ReactNode } from 'react'
 import { NextPage } from 'next'
 
-type NextConfig = {
-  publicRuntimeConfig: {
-    NODE_ENV: string
-    /**
-     * version of the app
-     */
-    VERSION: string
-  }
-  serverRuntimeConfig: Record<string, unknown>
-}
-
 declare type NextPageType = NextPage & { layout: ReactNode }
-
-declare module 'next/config' {
-  export default (): NextConfig => ({
-    publicRuntimeConfig: RuntimeConfig,
-  })
-}
